de949815c73710efd29f5d385376a7f61bb9ad8a
Gosha2015 <avtozip52@yandex.ru>
2016-02-04 00:43:45 +0300

    commit new version(v. 2.1.0.2)

---
+++ opencart/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://avtozip52.ru/opencart/admin/');
+define('HTTP_CATALOG', 'http://avtozip52.ru/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://avtozip52.ru/opencart/admin/');
+define('HTTPS_CATALOG', 'http://avtozip52.ru/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/admin/');
+define('DIR_SYSTEM', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/');
+define('DIR_LANGUAGE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/admin/language/');
+define('DIR_TEMPLATE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/admin/view/template/');
+define('DIR_CONFIG', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/config/');
+define('DIR_IMAGE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/image/');
+define('DIR_CACHE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/download/');
+define('DIR_LOGS', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/upload/');
+define('DIR_CATALOG', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db.cbs18.mass.hc.ru');
+define('DB_USERNAME', 'cbs18_avtozip52');
+define('DB_PASSWORD', 'see6Mahp');
+define('DB_DATABASE', 'wwwcbs18ru_avtozip52');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ opencart/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://avtozip52.ru/opencart/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://avtozip52.ru/opencart/');
+
+// DIR
+define('DIR_APPLICATION', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/catalog/');
+define('DIR_SYSTEM', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/');
+define('DIR_LANGUAGE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/config/');
+define('DIR_IMAGE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/image/');
+define('DIR_CACHE', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/cache/');
+define('DIR_DOWNLOAD', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/download/');
+define('DIR_LOGS', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/logs/');
+define('DIR_MODIFICATION', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/modification/');
+define('DIR_UPLOAD', '/www/cbs18/users/cbs18-auto15net/www/htdocs/opencart/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'db.cbs18.mass.hc.ru');
+define('DB_USERNAME', 'cbs18_avtozip52');
+define('DB_PASSWORD', 'see6Mahp');
+define('DB_DATABASE', 'wwwcbs18ru_avtozip52');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
